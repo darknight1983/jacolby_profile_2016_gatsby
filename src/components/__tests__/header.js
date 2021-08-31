50af9e451228b2e67cc32405066efdee3cc70c15
Jacolby Green <darknightnov@gmail.com>
2021-08-31 16:54:29 -0500

    Testing using @testing-library/react

---
+++ src/components/__tests__/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {render} from "@testing-library/react"
+
+
+import Header from "../header"
+
+
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+test("Displays the correct title", () => {
+  const { getByTestId} = render(<Title/>)
+  // Assertion
+  expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome");
+})
